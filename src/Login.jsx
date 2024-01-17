a2ebd13a6c589b09d19992f9e538f98b34d0b7a9
gavand-sagar <gavand.sagar.s@gmail.com>
2024-01-17 19:57:52 +0530

    lets push

---
+++ src/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Login() {
+    return (
+        <div className='login-container'>
+            <div className='login-form'>
+                <input />
+                <input />
+                <button>LOGIN</button>
+            </div>
+        </div>
+    )
+}
